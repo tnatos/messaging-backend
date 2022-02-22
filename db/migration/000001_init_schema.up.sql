20998360706f771bfe37787603e51e5ada3b9c05
binishmaharjan <legendbinish124@gmail.com>
2022-02-22 11:28:51 +0900

    fix database schema and add foreign keys to table

--- db/migration/000001_init_schema.up.sql
+++ db/migration/000001_init_schema.up.sql
@@ -1,25 +1,35 @@
-CREATE TABLE "users" (
-  "id" bigint PRIMARY KEY AUTO_INCREMENT,
-  "username" varchar(255),
-  "email" varchar(255),
-  "password" varchar(255)
+CREATE TABLE IF NOT EXISTS "users" (
+  "id" bigserial PRIMARY KEY,
+  "username" varchar NOT NULL,
+  "email" varchar NOT NULL,
+  "password" varchar NOT NULL
 );
 
 CREATE TABLE "members" (
-  "id" bigint PRIMARY KEY,
-  "user_id" bigint,
-  "room_id" bigint
+  "id" bigserial PRIMARY KEY,
+  "user_id" bigint NOT NULL,
+  "room_id" bigint NOT NULL
 );
 
 CREATE TABLE "rooms" (
-  "id" bigint PRIMARY KEY
+  "id" bigserial PRIMARY KEY
 );
 
 CREATE TABLE "messsages" (
-  "id" bigint PRIMARY KEY,
-  "room_id" bigint,
-  "sent" timestamptz,
-  "sender_id" bigint,
-  "seen_user_id" bigint,
-  "messsages" varchar(255)
+  "id" bigserial PRIMARY KEY,
+  "room_id" bigint NOT NULL,
+  "sent" timestamptz NOT NULL DEFAULT (now()),
+  "sender_id" bigint NOT NULL,
+  "seen_user_id" bigint NOT NULL,
+  "messsages" varchar NOT NULL
 );
+
+AlTER TABLE "members" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+AlTER TABLE "members" ADD FOREIGN KEY ("room_id") REFERENCES "rooms" ("id");
+
+AlTER TABLE "messsages" ADD FOREIGN KEY ("room_id") REFERENCES "rooms" ("id");
+
+AlTER TABLE "messsages" ADD FOREIGN KEY ("sender_id") REFERENCES "users" ("id");
+
+AlTER TABLE "messsages" ADD FOREIGN KEY ("seen_user_id") REFERENCES "users" ("id");
