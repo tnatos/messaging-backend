368084ed043664eaec983aa520bb56d5249be479
binishmaharjan <legendbinish124@gmail.com>
2022-03-08 13:04:51 +0900

    create a http server and run handler in main method

--- main.go
+++ main.go
@@ -1,15 +1,60 @@
 package main
 
 import (
+	"context"
+	"log"
+	"messaging-backend/handler"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	log.Print("Starting Server...")
+
+	router := gin.Default()
+
+	config := &handler.Config{
+		R: router,
+	}
+	handler.NewHandler(config)
+
+	// Create a http server
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
+	}
+
+	// Garceful server shutdown
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to initialize server: %v\n", err)
+		}
+	}()
+
+	log.Printf("Listening on port %v\n", server.Addr)
+
+	// Wait for kill signal of channel
+	quit := make(chan os.Signal)
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	// This blocks waits until a signal is passed into the quit channel
+	<-quit
+
+	// The context is used to inform the server it has 2 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	defer cancel()
+
+	// Shutdown server
+	log.Println("Shutting down server...")
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v\n", err)
+	}
 }
